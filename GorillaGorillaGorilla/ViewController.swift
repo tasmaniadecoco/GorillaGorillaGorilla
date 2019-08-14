a9d8b20808cd156626532bdfcf00eb6308177938
tasmaniadecoco <tasmaniadecoco@gmail.com>
2019-08-14 14:52:54 +0900

    add: first commit

---
+++ GorillaGorillaGorilla/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GorillaGorillaGorilla
+//
+//  Created by Yuki Fujita on 2019/08/14.
+//  Copyright © 2019 tasmaniadecoco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
